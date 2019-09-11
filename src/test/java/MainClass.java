13a570d858bf45ec24a7ed48733e749e8492389c
solovev <sereegey@yandex.ru>
2019-09-11 20:55:49 +0400

    Почищен код

--- src/test/java/MainClass.java
+++ src/test/java/MainClass.java
@@ -7,19 +7,30 @@ public class MainClass {
     private static final String USERNAME = "dexautomation";
     private static final String PASSWORD = "dexautomation";
     private static final String URL = "jdbc:mysql://db4free.net:3306/dexautomation";
-    private static String studentFirsnName = "Vladimir";
-    private static String studentLastName = "Sidorov";
-    private static Integer studentAge = 20;
-    private static Long studentPhone = 89121234567L;
+    private static String studentFirsnName = "Vlad";
+    private static String studentLastName = "Smirnov";
+    private static Integer studentAge = 30;
+    private static Long studentPhone = 89121237654L;
     public static void main (String[] args) throws SQLException {
         ConnectDatabase dbconnect = new ConnectDatabase();
         Connection connection = dbconnect.getConnection(URL, USERNAME, PASSWORD);
+        try {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+        } catch (SQLException ex) {
+            System.out.println("Ошибка регистрации драйвера");
+            return;
+        }
+        try (Connection connect = DriverManager.getConnection(URL, USERNAME, PASSWORD); Statement statement = connect.createStatement()) {
+            statement.execute("insert into dexautomation.Students (firstName, lastName, age, phone) values (\"Vlad\", \"Smirnov\", 30, 89121237654)");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
         String query = "select * from dexautomation.Students where age = " +
                 studentAge + " and phone = " + studentPhone + " and firstName like '" +
                 studentFirsnName + "' and lastName like '" + studentLastName + "'";
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(query);
-        //if (resultSet.next()){
             while (resultSet.next()){
                 String firstName;
                 String lastName;
@@ -32,24 +43,7 @@ public class MainClass {
                 Students students = new Students(firstName, lastName, age, phone);
                 System.out.println(students);
             }
-      //  }
-       // else{
-       //     System.out.println("Искомая запись не найдена");
-       // }
-
         statement.close();
         connection.close();
-        /*try {
-            Driver driver = new FabricMySQLDriver();
-            DriverManager.registerDriver(driver);
-        } catch (SQLException ex) {
-            System.out.println("Ошибка регистрации драйвера");
-            return;
-        }
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); Statement statement = connection.createStatement()) {
-            statement.execute("insert into dexautomation.Students (firstName, lastName, age, phone) values (\"Vladimir\", \"Sidorov\", 20, 89121234567)");
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }*/
     }
 }
